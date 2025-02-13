37c06d834aaff793e82caa731cac0c1ecd79d5d6
plastoid501 <plastoid501@gmail.com>
2025-02-13 22:20:35 +0900

    add lockedHorizontal & lockedVertical (all version)

--- src/main/java/net/plastoid501/ams/AdvancedMouseSensitivity.java
+++ src/main/java/net/plastoid501/ams/AdvancedMouseSensitivity.java
@@ -42,7 +42,6 @@ public class AdvancedMouseSensitivity implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 	}
 
 
